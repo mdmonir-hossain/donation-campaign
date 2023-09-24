a2790982c9166d3dc08f9a62b184c0aafdbc61db
mdmonir-hossain <136880861+mdmonir-hossain@users.noreply.github.com>
2023-09-24 20:41:08 +0600

    createBrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const donationCreateRoute = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <section>
+        <div>nav</div>
+        <Outlet></Outlet>
+      </section>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <div>Home</div>,
+      },
+      {
+        path: "/donation",
+        element: <div>Donation</div>,
+      },
+      {
+        path: "/statistics",
+        element: <div>Statistics</div>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    
-  </React.StrictMode>,
-)
+    <RouterProvider router={donationCreateRoute}></RouterProvider>
+  </React.StrictMode>
+);
